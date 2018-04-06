54fcee42b9de2e6dfd9fe0da6975ae561b3d3b4e
GO-MADsolutions <gomadsolutions@gmail.com>
2018-04-06 20:43:26 +0530

    Test Module Added

--- build/models/Test.js
+++ build/models/Test.js
@@ -1,3 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 /**
  * Created by gpalani on 02-02-2018.
  */
+const MongoModel = require('mongo-models');
+const Joi = require('joi');
+class Test extends MongoModel {
+}
+exports.Test = Test;
+Test.collection = 'tests';
+Test.schema = Joi.object().keys({
+    rollNumber: Joi.string().optional(),
+    subject: Joi.string().optional(),
+    date: Joi.string().optional(),
+    mark: Joi.string().optional(),
+    fullMark: Joi.string().optional(),
+});
+module.exports = Test;
