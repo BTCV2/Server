53aa48c599e7e917fa2478605aee67cedba0ec60
GO-MADsolutions <gomadsolutions@gmail.com>
2018-06-01 13:49:26 +0530

    Bd changes1

--- build/controllers/UserController.js
+++ build/controllers/UserController.js
@@ -11,13 +11,20 @@ class UserController {
     constructor() {
         this.createToken = (user) => {
             let scopes;
+            let firstLogin;
+            if (user.firstLogin === 'Y') {
+                firstLogin = 'Y';
+            }
+            else if (user.firstLogin !== 'Y') {
+                firstLogin = 'N';
+            }
             if (user.role === 'admin') {
                 scopes = 'admin';
             }
             else {
                 scopes = 'student';
             }
-            return jwt.sign({ id: user._id, username: user.userName, scope: scopes }, 'BTC', { algorithm: 'HS256', expiresIn: "1h" });
+            return jwt.sign({ id: user._id, username: user.userName, scope: scopes, firstLogin: firstLogin }, 'BTC', { algorithm: 'HS256', expiresIn: "1h" });
         };
     }
     insertUser(request, reply) {
@@ -28,6 +35,7 @@ class UserController {
                 }
                 else {
                     const User = request.server.plugins['hapi-mongo-models'].User;
+                    request.payload.firstLogin = 'Y';
                     request.payload.password = hash;
                     User.insertOne(request.payload, function (err, success) {
                         if (err) {
@@ -60,17 +68,26 @@ class UserController {
         });
     }
     updateUser(request, reply) {
-        const User = request.server.plugins['hapi-mongo-models'].User;
-        const filter = {
-            "userName": request.params.userName
-        };
-        User.findOneAndUpdate(filter, request.payload, function (err, success) {
-            if (err) {
-                reply().code(500);
-            }
-            else {
-                reply(success);
-            }
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(request.payload.password, salt, (err, hash) => {
+                if (err) {
+                }
+                else {
+                    const User = request.server.plugins['hapi-mongo-models'].User;
+                    const filter = {
+                        "userName": request.params.userName
+                    };
+                    request.payload.password = hash;
+                    User.findOneAndUpdate(filter, request.payload, function (err, success) {
+                        if (err) {
+                            reply().code(500);
+                        }
+                        else {
+                            reply(success);
+                        }
+                    });
+                }
+            });
         });
     }
     deleteUser(request, reply) {
@@ -92,6 +109,7 @@ class UserController {
         const filter = {
             "userName": request.payload.userName
         };
+        console.log('fileter', filter);
         User.findOne(filter, function (err, success) {
             if (err) {
                 reply(err).code(500);
@@ -102,13 +120,19 @@ class UserController {
             else {
                 /* reply(success).code(200)*/
                 bcrypt.compare(request.payload.password, success.password, (err, isValid) => {
+                    console.log("request.payload.password", request.payload.password);
+                    console.log("success.password", success.password);
+                    if (err) {
+                        console.log(err);
+                        reply(err);
+                    }
                     if (isValid) {
                         /* reply(success);*/
                         let self = new UserController();
                         reply({ id_token: self.createToken(success) }).code(201);
                     }
                     else {
-                        reply(Boom.badRequest('Incorrect password!'));
+                        reply(Boom.badRequest('Incorrect password! Please Check'));
                     }
                 });
             }
